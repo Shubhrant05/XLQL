4061cc277e0735f78b8e3fab3ad6fcc86a9c17c4
Shubhrant Yadav <shubhrantyadav34366@gmail.com>
2025-08-07 01:35:58 +0530

    implemented [insert] table command

--- xlql/cli.py
+++ xlql/cli.py
@@ -1,5 +1,5 @@
 import argparse
-from xlql.commands import create_db, list_db, delete_db
+from xlql.commands import create_db, list_db, delete_db, insert
 from xlql.core.utils import get_base_db_location, add_base_db_location
 def main():
     base_db_location = get_base_db_location()
@@ -20,8 +20,14 @@ def main():
     list_parser = subparsers.add_parser("listdb", help="List all the existing database")
     list_parser.set_defaults(func=list_db.main)
 
+    #drop db command
     drop_parser = subparsers.add_parser("deletedb", help="Deletes the chosen database")
     drop_parser.set_defaults(func=delete_db.main)
+
+    #insert table command
+    insert_parser = subparsers.add_parser("insert", help="Insert the file in the selected DB")
+    insert_parser.set_defaults(func=insert.main)
+
     args = parser.parse_args()
 
     if hasattr(args, "func"):
